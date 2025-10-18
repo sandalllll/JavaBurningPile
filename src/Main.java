8fcdf1737efa8de35ec030890542b0a30e638d2a
sandalllll <alexey.dm327@gmail.com>
2025-10-18 18:15:25 +0700

    N3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main
 {
@@ -76,5 +78,52 @@ public class Main
             remThread.start();
         }
         Thread.sleep(1000);
+        //N3
+        {
+            System.out.println("\nN3");
+            System.out.print("input orders count: ");
+            Scanner s = new Scanner(System.in);
+            int n = Integer.parseInt(s.nextLine());
+            ShoeWarehouse sw = new ShoeWarehouse();
+
+            ExecutorService ex = Executors.newFixedThreadPool(2);
+
+            ex.submit
+            (
+                () ->
+                {
+                    Random r = new Random();
+                    for(int i = 0; i < n; i++)
+                    {
+                        ShoeWarehouse.Order ord = new ShoeWarehouse.Order();
+                        ord.id = r.nextInt();
+                        ord.type = r.nextInt();
+                        ord.amount = r.nextInt();
+                        try {
+                            sw.receiveOrder(ord);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            );
+
+            ex.submit
+            (
+                () ->
+                {
+                    for(int i = 0; i < n; i++)
+                    {
+                        try {
+                            sw.fulfillOrder();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            );
+
+            ex.shutdown();
+        }
     }
 }
